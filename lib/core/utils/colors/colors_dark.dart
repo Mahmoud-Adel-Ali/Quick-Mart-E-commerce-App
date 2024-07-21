1a5875edf309437bb457dd221db4758215c90e20
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-21 14:31:46 -0700

    add all color of app

--- lib/core/utils/colors/colors_dark.dart
+++ lib/core/utils/colors/colors_dark.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ColorsDark {
-  static const Color mainColor = Color(0xFF1C1B1B);
-  static const Color buttonColor = Color(0xff21D4B4);
-  static const Color textColorWithOpacity = Color(0xffA2A2A6);
-  static const Color textColor = Color(0xffFFFFFF);
-  static const Color onBordingContainerColor = Color(0xff212322);
+  // brand color
+  static const Color cyan = Color(0xFF21D4B4);
+  static const Color cyan50 = Color(0xFF212322);
+  static const Color black = Color(0xFF1C1B1B);
+  static const Color white = Color(0xFFFFFFFF);
+  // grey color
+  static const Color grey50 = Color(0xFF282828);
+  static const Color grey100 = Color(0xFFC0C0C0);
+  static const Color grey150 = Color(0xFFA2A2A6);
+  // General
+  static const Color red = Color(0xFFEE4D4D);
+  static const Color cyan50General = Color(0xFF08E488);
+  static const Color blue = Color(0xFF1F8BDA);
+  static const Color purple = Color(0xFF4F1FDA);
+  static const Color yellow = Color(0xFFEBEF14);
+  static const Color orange = Color(0xFFF0821D);
+  static const Color merigold = Color(0xFFFFCB45);
+  static const Color browen = Color(0xFF5A1A05);
+  static const Color pink = Color(0xFFCE1DEB);
 }
--- lib/core/utils/colors/colors_light.dart
+++ lib/core/utils/colors/colors_light.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ColorsLight {
-  static const Color mainColor = Color(0xffFFFFFF);
-  static const Color buttonColor = Color(0xff1C1B1B);
-  static const Color textColorWithOpacity = Color(0xff6F7384);
-  static const Color textColor = Color(0xff1C1B1B);
-  static const Color onBordingContainerColor = Color(0xffF4FDFA);
+  // brand color
+  static const Color cyan = Color(0xFF21D4B4);
+  static const Color cyan50 = Color(0xFFF4FDFA);
+  static const Color black = Color(0xFF1C1B1B);
+  static const Color white = Color(0xFFFFFFFF);
+  // grey color
+  static const Color grey50 = Color(0xFFF4F5FD);
+  static const Color grey100 = Color(0xFFC0C0C0);
+  static const Color grey150 = Color(0xFF6F7384);
+  // General
+  static const Color red = Color(0xFFEE4D4D);
+  static const Color cyan50General = Color(0xFF08E488);
+  static const Color blue = Color(0xFF1F8BDA);
+  static const Color purple = Color(0xFF4F1FDA);
+  static const Color yellow = Color(0xFFEBEF14);
+  static const Color orange = Color(0xFFF0821D);
+  static const Color merigold = Color(0xFFFFCB45);
+  static const Color browen = Color(0xFF5A1A05);
+  static const Color pink = Color(0xFFCE1DEB);
 }
