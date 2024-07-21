6c658094dd7d9554a0bd92dddab1a99e4a7c14ff
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-20 20:07:19 -0700

    onboarding ui done

--- lib/features/onBoarding/presentation/widgets/on_boarding_view_body.dart
+++ lib/features/onBoarding/presentation/widgets/on_boarding_view_body.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:quick_mart_app/core/extensions/context_extention.dart';
+import 'package:quick_mart_app/core/utils/colors/colors_dark.dart';
+import 'package:quick_mart_app/features/onBoarding/presentation/widgets/onboading_first_view.dart';
 import 'package:quick_mart_app/features/onBoarding/presentation/widgets/onboarding_secode_view.dart';
 import 'package:quick_mart_app/features/onBoarding/presentation/widgets/onbording_third_view.dart';
 
@@ -8,11 +12,19 @@ class OnBoardingViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      // child: OnBoardingFirstView(),
-      // child: OnBoardingSecodeView(),
-      child: OnBoardingThirdView(),
+    return ImageSlideshow(
+      initialPage: 0,
+      height: double.infinity,
+      onPageChanged: (value) {},
+      disableUserScrolling: true,
+      indicatorColor:ColorsDark.buttonColor,
+      indicatorRadius: 6,
+      indicatorBackgroundColor: context.color.textColorWithOpacity,
+      children: const [
+        OnBoardingFirstView(),
+        OnBoardingSecodeView(),
+        OnBoardingThirdView(),
+      ],
     );
   }
 }
-
