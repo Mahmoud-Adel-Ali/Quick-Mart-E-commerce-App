613adbc20cd9fc794cfb69245dac0180d73cd693
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-21 21:28:52 -0700

    custom dropdown done

---
+++ lib/core/app/widgets/custom_drop_down.dart
@@ -0,0 +1,74 @@
+import 'package:animated_custom_dropdown/custom_dropdown.dart';
+import 'package:flutter/material.dart';
+import 'package:quick_mart_app/core/extensions/context_extention.dart';
+import 'package:quick_mart_app/core/utils/styles/styles.dart';
+
+class CustomDropdownFeild extends StatefulWidget {
+  const CustomDropdownFeild({
+    super.key,
+    required this.hintText,
+    required this.items,
+    this.controller,
+  });
+  final String hintText;
+  final List<String> items;
+  final SingleSelectController<String?>? controller;
+
+  @override
+  State<CustomDropdownFeild> createState() => _CustomDropdownFeildState();
+}
+
+class _CustomDropdownFeildState extends State<CustomDropdownFeild> {
+  bool selectedItem = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomDropdown<String>(
+      hintText: widget.hintText,
+      decoration: CustomDropdownDecoration(
+        expandedFillColor: context.color.mainColor,
+        closedFillColor: context.color.mainColor,
+        hintStyle: Styless.textRegular12(context),
+        headerStyle: Styless.textRegular14(context),
+        // listItemStyle: Styless.textRegular12(context),
+        expandedBorder: Border.all(color: context.color.cyan),
+        closedBorder: Border.all(
+            color: selectedItem ? context.color.blue : context.color.mainColor),
+        closedSuffixIcon: Icon(
+          Icons.keyboard_arrow_down,
+          color: context.color.textColor,
+        ),
+        expandedSuffixIcon: Icon(
+          Icons.keyboard_arrow_up,
+          color: context.color.textColor,
+        ),
+      ),
+      controller: widget.controller,
+      items: widget.items,
+      initialItem: null,
+      listItemBuilder: (context, item, isSelected, onItemSelect) {
+        return Container(
+          padding: const EdgeInsets.only(top: 10),
+          decoration: BoxDecoration(
+            border: Border(
+              top: BorderSide(
+                color: widget.items.indexOf(item) != 0
+                    ? context.color.grey50
+                    : Colors.transparent,
+              ),
+            ),
+          ),
+          child: Text(
+            item,
+            style: Styless.textRegular12(context),
+          ),
+        );
+      },
+      onChanged: (value) {
+        // log('changing value to: $value');
+        selectedItem = true;
+        setState(() {});
+      },
+    );
+  }
+}
--- lib/quick_mart.dart
+++ lib/quick_mart.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart_app/core/app/cubit/app_cubit.dart';
-import 'package:quick_mart_app/core/app/widgets/custom_text_form_field.dart';
+import 'package:quick_mart_app/core/app/widgets/custom_drop_down.dart';
 import 'package:quick_mart_app/core/databases/cach_keys.dart';
 import 'package:quick_mart_app/core/databases/my_cach-helper.dart';
-import 'package:quick_mart_app/core/routes/app_routes.dart';
 import 'package:quick_mart_app/core/utils/theme/app_theme.dart';
 
 class QuickMart extends StatelessWidget {
@@ -21,8 +20,7 @@ class QuickMart extends StatelessWidget {
         builder: (context, state) {
           return MaterialApp(
             debugShowCheckedModeBanner: false,
-            theme: context.read<AppCubit>().isDark ?
-             themeDark() : themeLight(),
+            theme: context.read<AppCubit>().isDark ? themeDark() : themeLight(),
             // initialRoute: AppRoutes.splash,
             // onGenerateRoute: AppRoutes.onGenerateRoute,
             home: const AppWidgets(),
@@ -32,16 +30,25 @@ class QuickMart extends StatelessWidget {
     );
   }
 }
+
 class AppWidgets extends StatelessWidget {
   const AppWidgets({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
+    return const Scaffold(
       body: Center(
         child: Padding(
           padding: EdgeInsets.all(18.0),
-          child: CustomTextFormField(hintText: 'hint', lableText: 'Lable',),
+          child: CustomDropdownFeild(
+            hintText: 'Select city',
+            items: [
+              'Karachi',
+              'Hyderabad',
+              'Sukkur',
+              'Egypt',
+            ],
+          ),
         ),
       ),
     );
