acdeac39238d884fa77b2c2aa1abfaf39a49743e
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-23 19:17:45 -0700

    search field done

--- lib/core/app/widgets/custom_search_field.dart
+++ lib/core/app/widgets/custom_search_field.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:quick_mart_app/core/app/widgets/search_field_icon.dart';
+import 'package:quick_mart_app/core/extensions/context_extention.dart';
+import 'package:quick_mart_app/core/utils/images/app_images.dart';
+import 'package:quick_mart_app/core/utils/styles/styles.dart';
 
-class CustomSearchField extends StatelessWidget {
-  const CustomSearchField({super.key});
+class CustomSearchField extends StatefulWidget {
+  const CustomSearchField({super.key, this.controller, this.onChanged});
+  final TextEditingController? controller;
+  final void Function(String)? onChanged;
+  @override
+  State<CustomSearchField> createState() => _CustomSearchFieldState();
+}
+
+class _CustomSearchFieldState extends State<CustomSearchField> {
+  bool isAvailable = false;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Card(
+      color: context.color.mainColor,
+      elevation: 5,
+      shadowColor: context.color.grey50,
+      child: TextFormField(
+        style: Styless.textMedium16(context),
+        controller: widget.controller,
+        onTap: () {
+          isAvailable = true;
+          setState(() {});
+        },
+        onFieldSubmitted: (s) {
+          isAvailable = false;
+          setState(() {});
+        },
+        onChanged:widget.onChanged,
+        decoration: InputDecoration(
+            hintText: 'Search',
+            hintStyle: Styless.textRegular12(context),
+            suffixIcon: SearchFieldIcon(
+                icon: isAvailable
+                    ? context.image.suffSerchField
+                    : Assets.imagesSuffSearchGrey),
+            prefixIcon: SearchFieldIcon(
+                icon: isAvailable
+                    ? context.image.searchIcon
+                    : Assets.imagesSearchGrey),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(18),
+              borderSide: BorderSide(color: context.color.cyan),
+            ),
+            border: InputBorder.none),
+      ),
+    );
   }
 }
---
+++ lib/core/app/widgets/search_field_icon.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class SearchFieldIcon extends StatelessWidget {
+  const SearchFieldIcon({
+    super.key, required this.icon,
+  });
+  final String icon;
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(icon,fit: BoxFit.scaleDown,);
+  }
+}
