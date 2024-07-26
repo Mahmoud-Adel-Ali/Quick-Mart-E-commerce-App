20a3699f78ab6f41e12946844b0e153a2fb41398
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-26 14:28:07 -0700

    create email verification view header

---
+++ lib/features/auth/presentation/widgets/email_verification_header.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart_app/core/extensions/context_extention.dart';
+import 'package:quick_mart_app/core/utils/styles/styles.dart';
+
+class EmaillVerificationHeader extends StatelessWidget {
+  const EmaillVerificationHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      title: Text(
+        'Email Verification',
+        style: Styless.textBold24(context),
+      ),
+      subtitle: Padding(
+        padding: const EdgeInsets.only(top:  20.0),
+        child: Text(
+          'Enter the 6-digit verification code send to your email address.',
+          style: Styless.textMedium14(context).copyWith(color: context.color.grey150),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/auth/presentation/widgets/email_verification_view_body.dart
+++ lib/features/auth/presentation/widgets/email_verification_view_body.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:quick_mart_app/features/auth/presentation/widgets/email_verification_header.dart';
 
 class EmailVerificationViewBody extends StatelessWidget {
   const EmailVerificationViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      
+    return const Padding(
+      padding: EdgeInsets.all(18.0),
+      child: Column(
+        children: [
+          EmaillVerificationHeader(),
+        ],
+      ),
     );
   }
 }
