51256a6ab4017506fff30a24f359353ebecfbd47
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-25 20:02:27 -0700

    add login form

---
+++ lib/features/auth/presentation/widgets/login_form.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart_app/core/app/widgets/custom_button.dart';
+import 'package:quick_mart_app/core/app/widgets/custom_text_form_field.dart';
+import 'package:quick_mart_app/core/app/widgets/custom_toast_message.dart';
+import 'package:quick_mart_app/core/extensions/context_extention.dart';
+import 'package:quick_mart_app/core/utils/styles/styles.dart';
+import 'package:quick_mart_app/features/auth/manager/functions/valid.dart';
+
+class LoginForm extends StatefulWidget {
+  const LoginForm({super.key});
+
+  @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  bool showPassword = true;
+  GlobalKey<FormState> loginFormKey = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: loginFormKey,
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          CustomTextFormField(
+            hintText: 'Enter Your Email',
+            lableText: 'Email',
+            validator: (value) {
+              return validatorOfEmail(value);
+            },
+          ),
+          const SizedBox(height: 20),
+          CustomTextFormField(
+            hintText: 'Enter Your Password',
+            lableText: 'Password',
+            obscureText: showPassword,
+            validator: (value) {
+              return validatorOfPassword(value);
+            },
+            suffixIcon: IconButton(
+              onPressed: () {
+                showPassword = !showPassword;
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.visibility,
+                color: context.color.textColor,
+              ),
+            ),
+          ),
+          const SizedBox(height: 20),
+          Container(
+            width: double.infinity,
+            alignment: Alignment.centerRight,
+            child: TextButton(
+              onPressed: () {},
+              child: Text(
+                'Forgot password?',
+                style: Styless.textSemiBold14(context)
+                    .copyWith(color: context.color.cyan),
+              ),
+            ),
+          ),
+          const SizedBox(height: 50),
+          CustomButton(
+            onPressed: () {
+              if (loginFormKey.currentState!.validate()) {
+                CustomToastMessage().topToast(context,
+                    msg: 'Success : Done', type: ToastMessageType.success);
+              } else {
+                CustomToastMessage().bottomToast(context,
+                    msg: 'Error : fill all fields',
+                    type: ToastMessageType.error);
+              }
+            },
+            text: 'Login',
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/auth/presentation/widgets/login_view_body.dart
+++ lib/features/auth/presentation/widgets/login_view_body.dart
@@ -1,15 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:quick_mart_app/core/app/widgets/custom_quick_mart_header.dart';
+import 'package:quick_mart_app/core/extensions/context_extention.dart';
+import 'package:quick_mart_app/core/routes/app_routes.dart';
+import 'package:quick_mart_app/core/utils/images/app_images.dart';
+import 'package:quick_mart_app/core/utils/styles/styles.dart';
+import 'package:quick_mart_app/features/auth/presentation/widgets/login_form.dart';
 
 class LoginViewBody extends StatelessWidget {
   const LoginViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Column(
-        children: [
-          
-        ],
+    final height = MediaQuery.sizeOf(context).height;
+    return  SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            CustomQuickMartHeader(height: height),
+            const SizedBox(height: 30),
+            Container(
+                alignment: Alignment.centerLeft,
+                width: double.infinity,
+                child: Text(
+                  "Login",
+                  style: Styless.textBold24(context),
+                )),
+            const SizedBox(height: 5),
+            Row(
+              children: [
+                Text(
+                  'Don’t have an account? ',
+                  style: Styless.textRegular14(context),
+                ),
+                TextButton(
+                  onPressed: () {
+                    context.pushReplacementNamed(AppRoutes.signUp);
+                  },
+                  child: Text(
+                    'Signup ',
+                    style: Styless.textRegular14(context)
+                        .copyWith(color: context.color.cyan),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 30),
+            const LoginForm(),
+            const SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Login with Google',
+                  style: Styless.textSemiBold16(context),
+                ),
+                const SizedBox(width: 20),
+                InkWell(
+                  onTap: () {},
+                  child: SvgPicture.asset(Assets.imagesGoogle),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
