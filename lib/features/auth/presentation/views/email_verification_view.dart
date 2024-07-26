4873f4854ff9a2cd5e62484a8b7ee328da3e7945
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-26 14:17:57 -0700

    create email verification view app bar

--- lib/features/auth/presentation/views/email_verification_view.dart
+++ lib/features/auth/presentation/views/email_verification_view.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:quick_mart_app/core/extensions/context_extention.dart';
+import 'package:quick_mart_app/core/utils/styles/styles.dart';
+import 'package:quick_mart_app/features/auth/presentation/widgets/email_verification_view_body.dart';
 
 class EmailVerificationView extends StatelessWidget {
   const EmailVerificationView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Verification Code',
+          style: Styless.textSemiBold14(context),
+        ),
+        backgroundColor: context.color.mainColor,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: Icon(Icons.arrow_back,color: context.color.textColor,)),
+      ),
+      body: const EmailVerificationViewBody(),
+    );
   }
 }
---
+++ lib/features/auth/presentation/widgets/email_verification_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EmailVerificationViewBody extends StatelessWidget {
+  const EmailVerificationViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      
+    );
+  }
+}
