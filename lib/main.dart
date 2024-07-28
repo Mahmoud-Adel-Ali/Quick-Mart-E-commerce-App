0fe1d228bdab7f102853415dcd589a6c41cbb615
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-27 18:48:06 -0700

    set device not rotated

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:quick_mart_app/core/databases/my_cach-helper.dart';
 import 'package:quick_mart_app/quick_mart.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await CacheHelper().init();
-  runApp(const QuickMart());
-}
+// this step used to set device not rotated
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const QuickMart());
+  });}
