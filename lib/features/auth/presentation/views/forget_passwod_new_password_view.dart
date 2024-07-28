6a501018cf2ef60aaa229a6c91fe337b13b03cd4
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-27 17:09:48 -0700

    Forget Passwod New Password View done

--- lib/features/auth/presentation/views/forget_passwod_new_password_view.dart
+++ lib/features/auth/presentation/views/forget_passwod_new_password_view.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:quick_mart_app/core/utils/styles/styles.dart';
+import 'package:quick_mart_app/features/auth/presentation/widgets/custom_app_bar.dart';
+import 'package:quick_mart_app/features/auth/presentation/widgets/forget_passwod_new_password_form.dart';
+import 'package:quick_mart_app/features/auth/presentation/widgets/form_header.dart';
 
 class ForgetPasswodNewPasswordView extends StatelessWidget {
   const ForgetPasswodNewPasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                CustomAppBar(
+                  title: 'Create Password',
+                  trailing:
+                      Text('03/03', style: Styless.textSemiBold16(context)),
+                ),
+                const SizedBox(height: 50),
+                const FormHeader(
+                  leadingText: 'Enter your new password and remember it.',
+                  viewTitle: 'New Password',
+                ),
+                const SizedBox(height: 50),
+                const ForgetPasswodNewPasswordForm(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/auth/presentation/widgets/forget_passwod_new_password_form.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:quick_mart_app/core/app/widgets/custom_button.dart';
+import 'package:quick_mart_app/core/app/widgets/custom_text_form_field.dart';
+import 'package:quick_mart_app/core/app/widgets/custom_toast_message.dart';
+import 'package:quick_mart_app/core/extensions/context_extention.dart';
+import 'package:quick_mart_app/features/auth/manager/functions/valid.dart';
+
+class ForgetPasswodNewPasswordForm extends StatefulWidget {
+  const ForgetPasswodNewPasswordForm({super.key});
+
+  @override
+  State<ForgetPasswodNewPasswordForm> createState() => _ForgetPasswodNewPasswordFormState();
+}
+
+class _ForgetPasswodNewPasswordFormState extends State<ForgetPasswodNewPasswordForm> {
+  bool showPassword = true;
+  bool showConfirmPassword = true;
+  GlobalKey<FormState> nePasswordForm = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: nePasswordForm,
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+CustomTextFormField(
+            hintText: 'Enter Your Password',
+            lableText: 'Password',
+            obscureText: showPassword,
+            validator: (value) {
+              return validatorOfPassword(value);
+            },
+            suffixIcon: IconButton(
+              onPressed: () {
+                showPassword = !showPassword;
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.visibility,
+                color: context.color.textColor,
+              ),
+            ),
+          ),
+          
+          const SizedBox(height: 20),
+          CustomTextFormField(
+            hintText: 'Enter Your Confirm Password',
+            lableText: 'Confirm Password',
+            obscureText: showConfirmPassword,
+            validator: (value) {
+              return validatorOfPassword(value);
+            },
+            suffixIcon: IconButton(
+              onPressed: () {
+                showConfirmPassword = !showConfirmPassword;
+                setState(() {});
+              },
+              icon: Icon(
+                Icons.visibility,
+                color: context.color.textColor,
+              ),
+            ),
+          ),
+          const SizedBox(height: 50),
+          CustomButton(
+            onPressed: () {
+              if (nePasswordForm.currentState!.validate()) {
+                CustomToastMessage().topToast(context,
+                    msg: 'Success : Done', type: ToastMessageType.success);
+              } else {
+                CustomToastMessage().bottomToast(context,
+                    msg: 'Error : fill all fields',
+                    type: ToastMessageType.error);
+              }
+            },
+            text: 'Save',
+          ),
+        ],
+      ),
+    );
+  }
+
+}
