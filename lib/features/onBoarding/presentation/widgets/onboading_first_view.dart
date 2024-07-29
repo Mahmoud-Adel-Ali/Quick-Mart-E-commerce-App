d5dff84f1506ff999c6b3f726eccc87af45ca1e6
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-07-29 16:52:39 -0700

    refactor onboardling view part 2

--- lib/features/onBoarding/presentation/widgets/onboading_first_view.dart
+++ lib/features/onBoarding/presentation/widgets/onboading_first_view.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:quick_mart_app/core/app/widgets/custom_button.dart';
 import 'package:quick_mart_app/core/extensions/context_extention.dart';
-import 'package:quick_mart_app/core/routes/app_routes.dart';
 import 'package:quick_mart_app/core/utils/images/app_images.dart';
 import 'package:quick_mart_app/features/onBoarding/presentation/manager/cubit/onboarding_cubit.dart';
 import 'package:quick_mart_app/features/onBoarding/presentation/manager/cubit/onboarding_state.dart';
@@ -25,7 +24,7 @@ class OnBoardingFirstView extends StatelessWidget {
           height: 35,
         ),
         onTapOnSkipForNow: () {
-          context.pushReplacementNamed(AppRoutes.login);
+          context.read<OnbordingCubit>().toThirdPage();
         },
         image: Assets.imagesOnBoarding1,
         title: 'Explore a wide range of products',
--- lib/features/onBoarding/presentation/widgets/onboarding_secode_view.dart
+++ lib/features/onBoarding/presentation/widgets/onboarding_secode_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart_app/core/app/widgets/custom_button.dart';
 import 'package:quick_mart_app/core/extensions/context_extention.dart';
-import 'package:quick_mart_app/core/routes/app_routes.dart';
 import 'package:quick_mart_app/core/utils/images/app_images.dart';
 import 'package:quick_mart_app/features/onBoarding/presentation/manager/cubit/onboarding_cubit.dart';
 import 'package:quick_mart_app/features/onBoarding/presentation/manager/cubit/onboarding_state.dart';
@@ -30,7 +29,7 @@ class OnBoardingSecodeView extends StatelessWidget {
           ),
         ),
         onTapOnSkipForNow: () {
-          context.pushReplacementNamed(AppRoutes.login);
+          context.read<OnbordingCubit>().toThirdPage();
         },
         image: Assets.imagesOnBoarding2,
         title: 'Unlock exclusive offers and discounts',
