1e3aa0e2baf59f77be84b2f60b0963673906b52e
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-08-02 15:08:21 -0700

    custom colors of product

---
+++ lib/core/widgets/product/custom_product_colors.dart
@@ -0,0 +1,59 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:quick_mart_app/core/extensions/context_extention.dart';
+
+class CustomProductColors extends StatelessWidget {
+  const CustomProductColors({
+    super.key,
+    required this.productColors,
+    required this.currentColor,
+  });
+  final List<Color> productColors;
+  final int currentColor;
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 25,
+      width: (17 * productColors.length).toDouble(),
+      child: Stack(
+        alignment: Alignment.center,
+        children: productColors.reversed.toList().asMap().entries.map(
+          (e) {
+            int index = e.key;
+            Color color = e.value;
+            log(index.toString());
+            return Positioned(
+              right: index * 15,
+              child: OneColorsOfProduct(
+                color: color,
+                isSelected: index + 1 ==
+                    (currentColor - 1 - productColors.length).abs(),
+              ),
+            );
+          },
+        ).toList(),
+      ),
+    );
+  }
+}
+
+class OneColorsOfProduct extends StatelessWidget {
+  const OneColorsOfProduct(
+      {super.key, required this.color, required this.isSelected});
+  final Color color;
+  final bool isSelected;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: isSelected ? 26 : 24,
+      height: isSelected ? 26 : 24,
+      decoration: BoxDecoration(
+        color: color,
+        border: Border.all(
+            width: 2, color: isSelected ? context.color.blue : color),
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+}
--- lib/quick_mart.dart
+++ lib/quick_mart.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart_app/core/app/cubit/app_cubit.dart';
-import 'package:quick_mart_app/core/utils/app_routes.dart';
+import 'package:quick_mart_app/core/extensions/context_extention.dart';
 import 'package:quick_mart_app/core/theme/app_theme.dart';
-import 'package:quick_mart_app/core/widgets/product/custom_wish_icon.dart';
-import 'package:quick_mart_app/core/widgets/product/home_banner.dart';
+import 'package:quick_mart_app/core/widgets/product/custom_product_colors.dart';
 
 class QuickMart extends StatelessWidget {
   const QuickMart({super.key});
@@ -30,8 +29,16 @@ class MyWidgets extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: CustomWishIcon(),),
+    return Scaffold(
+      body: Center(
+          child: CustomProductColors(productColors: [
+        context.color.textColor,
+        context.color.cyan50General,
+        context.color.cyan,
+        context.color.red,
+        context.color.blue,
+        context.color.purple,
+      ], currentColor: 1)),
     );
   }
 }
