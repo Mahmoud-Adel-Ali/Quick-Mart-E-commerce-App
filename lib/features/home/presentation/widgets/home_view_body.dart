a23b89b5b3795351c94efc98ef4795a134aaa844
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-08-04 15:06:32 -0700

    home view

--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quick_mart_app/core/widgets/product/home_banner.dart';
 import 'package:quick_mart_app/features/home/presentation/widgets/home_category_section.dart';
+import 'package:quick_mart_app/features/home/presentation/widgets/latest_products_section.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
@@ -9,12 +10,16 @@ class HomeViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(12.0),
-      child: const Column(
-        children: [
-          HomeBanner(),
-          SizedBox(height: 12),
-          HomeCategorySection(),
-        ],
+      child: SingleChildScrollView(
+        child: const Column(
+          children: [
+            HomeBanner(),
+            SizedBox(height: 12),
+            HomeCategorySection(),
+            SizedBox(height: 12),
+            // LatestProductsSection(),
+          ],
+        ),
       ),
     );
   }
