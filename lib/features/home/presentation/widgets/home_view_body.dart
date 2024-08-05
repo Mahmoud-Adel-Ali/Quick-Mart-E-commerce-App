2af4fbbada0fadd7581f2325ff99a8b389d56986
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-08-04 17:41:56 -0700

    refactor home view

--- lib/features/home/presentation/widgets/home_view_body.dart
+++ lib/features/home/presentation/widgets/home_view_body.dart
@@ -1,26 +1,42 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quick_mart_app/core/widgets/product/custom_product_card.dart';
 import 'package:quick_mart_app/core/widgets/product/home_banner.dart';
+import 'package:quick_mart_app/features/home/presentation/widgets/header_of_any_home_section.dart';
 import 'package:quick_mart_app/features/home/presentation/widgets/home_category_section.dart';
-import 'package:quick_mart_app/features/home/presentation/widgets/latest_products_section.dart';
 
 class HomeViewBody extends StatelessWidget {
   const HomeViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(12.0),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            HomeBanner(),
-            SizedBox(height: 12),
-            HomeCategorySection(),
-            SizedBox(height: 12),
-            LatestProductsSection(),
-          ],
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: const HomeBanner()),
+          SliverToBoxAdapter(child: const SizedBox(height: 12)),
+          SliverToBoxAdapter(child: const HomeCategorySection()),
+          SliverToBoxAdapter(child: const SizedBox(height: 12)),
+          SliverToBoxAdapter(
+            child: HeaderOfAnyHomeSection(
+              title: 'Latest Products',
+              onPressedOnSeeAll: () {},
+            ),
+          ),
+          SliverToBoxAdapter(child: const SizedBox(height: 12)),
+          SliverGrid.builder(
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 0.65,
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+            ),
+            itemBuilder: (context, index) => const CustomProductCard(),
+          ),
+        ],
       ),
     );
-  }
+
+    }
 }
