609085f54422af52c92ae8a1693ee0400f2526a0
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-08-05 20:58:54 -0700

    refactor product cart

--- lib/core/widgets/product/custom_product_card.dart
+++ lib/core/widgets/product/custom_product_card.dart
@@ -12,6 +12,7 @@ class CustomProductCard extends StatelessWidget {
     const String imgPath =
         'https://s3-alpha-sig.figma.com/img/1b9b/7b5f/af2955a24f3eb00b24e6e50afd567e36?Expires=1723420800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ojtNIP4wDXU78FmAEOfFJM8yI1LJr6flriTxx-LdpermBe0bRDfkdmgiPZlpDgC8fhmoHbXBSn~fPxEalcSyjYKzh96VXgq2cWOa9dSAfA9w-TZByQvJLoT25t3BCmjuvLTDmlME92K2JFBSAjiu5nXHXBDDKdunmqbLxZT8nLQ6QxQAUtN6cw3Vp~xSqLhgRAfhdhvImG4-nlK4JUdQ-zlHlQ9jGNa9nSQB7LbhGzdjrB6hwSb9s0ad3wuGpRTWTgkN0uoR29tI7S5kjCmSacgLzC7KkV9b2UQfaOgyq4v0vkm-uFqejD88-2SU8QB6gvcfWVXIr9AAqgFQfUPdzw__';
     return Card(
+      color: context.color.mainColor,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -31,7 +32,7 @@ class CustomProductCard extends StatelessWidget {
                 child: Text(
                   'All 5 Colors',
                   style: Styless.textMedium14(context).copyWith(
-                      color: context.color.purple,
+                      color: context.color.grey150,
                       decoration: TextDecoration.underline),
                 ),
               ),
