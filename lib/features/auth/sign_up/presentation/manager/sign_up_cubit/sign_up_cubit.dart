50fce6ba264a2860e3e6b6739cc2a1f85fb2b62a
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-08-11 17:58:20 -0700

    sign up function

--- lib/features/auth/sign_up/presentation/manager/sign_up_cubit/sign_up_cubit.dart
+++ lib/features/auth/sign_up/presentation/manager/sign_up_cubit/sign_up_cubit.dart
@@ -1,8 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:quick_mart_app/features/auth/data/models/sign_up_user/sign_up_model.dart';
+import 'package:quick_mart_app/features/auth/data/repos/auth_repo_implementation.dart';
 
 part 'sign_up_state.dart';
 
 class SignUpCubit extends Cubit<SignUpState> {
-  SignUpCubit() : super(SignUpInitial());
+  SignUpCubit({required this.authRepoImplementation}) : super(SignUpInitial());
+  final AuthRepoImplementation authRepoImplementation;
+
+  
+  TextEditingController signUpUserName = TextEditingController();
+  TextEditingController signUpPhoneNumber = TextEditingController();
+  TextEditingController signUpEmail = TextEditingController();
+  TextEditingController signUpPassword = TextEditingController();
+  TextEditingController signUpConfirmPassword = TextEditingController();
+  GlobalKey<FormState> signUpFormKey = GlobalKey();
+
+  signUp() async {
+    emit(SignUpLoading());
+    final response = await authRepoImplementation.signUp(
+        username: signUpUserName.text,
+        phoneNumber: signUpPhoneNumber.text,
+        email: signUpEmail.text,
+        password: signUpPassword.text,
+        confirmPassword: signUpConfirmPassword.text);
+    response.fold(
+        (errorMessage) => emit(SignUpFailure(errorMessage: errorMessage)),
+        (signUpModel) => emit(SignUpSuccess(signUpModel: signUpModel)));
+  }
 }
