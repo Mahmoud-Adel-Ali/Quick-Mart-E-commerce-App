105d8d0b40f1fa3a02ca6c0009dd61900f85ee9d
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-08-12 14:07:53 -0700

    add login methode to cubit

--- lib/features/auth/login/presentation/manager/cubit/login_cubit.dart
+++ lib/features/auth/login/presentation/manager/cubit/login_cubit.dart
@@ -1,9 +1,27 @@
 
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart_app/features/auth/data/models/user_model/user_model.dart';
+import 'package:quick_mart_app/features/auth/data/repos/auth_repo_implementation.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(LoginInitial());
+  LoginCubit({required this.authRepoImplementation}) : super(LoginInitial());
+  final AuthRepoImplementation authRepoImplementation;
+
+  GlobalKey<FormState> logInformKey = GlobalKey();
+  TextEditingController loginEmail = TextEditingController();
+  TextEditingController loginPassword = TextEditingController();
+
+  login() async {
+    emit(LoginLoading());
+    final response = await authRepoImplementation.login(
+      email: loginEmail.text,
+      password: loginPassword.text,
+    );
+    response.fold(
+        (errorMessage) => emit(LoginFailure(errorMessage: errorMessage)),
+        (userModel) => emit(LoginSuccess(userModel: userModel)));
+  }
 }
