038e128511a080476bd15a9bdb5a7c83cb63beeb
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-08-14 16:21:27 -0700

    forget password view body pard 3

--- lib/features/auth/login/presentation/views/forget_password_view.dart
+++ lib/features/auth/login/presentation/views/forget_password_view.dart
@@ -3,6 +3,7 @@ import 'package:quick_mart_app/core/extensions/context_extention.dart';
 import 'package:quick_mart_app/core/utils/app_routes.dart';
 import 'package:quick_mart_app/core/utils/styles.dart';
 import 'package:quick_mart_app/core/widgets/custom_simple_app_bar.dart';
+import 'package:quick_mart_app/features/auth/login/presentation/widgets/forget_password_view_body.dart';
 
 class ForgetPasswordView extends StatelessWidget {
   const ForgetPasswordView({super.key});
@@ -15,6 +16,7 @@ class ForgetPasswordView extends StatelessWidget {
               Text('Forgot Password', style: Styless.textSemiBold14(context)),
           onTapOnLeading: () =>
               context.pushNamedAndRemoveUntil(AppRoutes.login)),
+      body: ForgetPasswordViewBody(),
     );
   }
 }
