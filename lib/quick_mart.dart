645359f81c4ae718926a31b60a900e9da39d4029
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-08-18 14:01:55 -0700

    refactor quick mart

--- lib/quick_mart.dart
+++ lib/quick_mart.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart_app/core/app/cubit/app_cubit.dart';
 import 'package:quick_mart_app/core/theme/app_theme.dart';
 import 'package:quick_mart_app/core/utils/app_routes.dart';
-import 'package:quick_mart_app/core/widgets/product/custom_size_of_product.dart';
 
 class QuickMart extends StatelessWidget {
   const QuickMart({super.key});
@@ -17,21 +16,8 @@ class QuickMart extends StatelessWidget {
           theme: context.read<AppCubit>().isDark ? themeDark() : themeLight(),
           initialRoute: AppRoutes.splash,
           onGenerateRoute: AppRoutes.onGenerateRoute,
-          // home: const SplashView(),
         );
       },
-    );
-  }
-}
-
-class MyWidgets extends StatelessWidget {
-  const MyWidgets({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-          child: CustomSizeOfProduct(productSize: ProductSize.m,)),
     );
   }
 }
