aac5694800a79b5edf936effddf8b6ebb2249c73
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-10-20 20:24:24 -0700

    refactor sub categories item

--- lib/core/widgets/product/custom_sub_categories_item.dart
+++ lib/core/widgets/product/custom_sub_categories_item.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:quick_mart_app/core/utils/styles.dart';
 import 'package:quick_mart_app/core/widgets/custom_cached_network_image.dart';
 
 class CustomSubCategoriesItem extends StatelessWidget {
@@ -10,16 +11,24 @@ class CustomSubCategoriesItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 160 / 138,
-      child: Container(
-        padding: EdgeInsets.only(right: 16, top: 16),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(24)),
-          image: customCachedNetworkImageprovider('https://s3-alpha-sig.figma.com/img/e247/c4b6/2d9352afd2a1a76e569e14ab01475e34?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bd9bwpmnwwbSgd0vUgtaJleJUm7RDiypSaM9eUJQWDJCIOnBTh2oB~uPHkgomWPkdJWDTO6FDehTZC73gI73-54-pjflldmWM6DAxT8ajAKGp2mCMc8e3KcQaP9gaC8kpLyKhxTWLHukIVAOaje-iJONoTTayzNSZ6EeeTSG9JG1LtmVWNJyhrH-NT-Et3oZQnGR28CXVWthIJigfeYbFWiZ1oQ8iWKGxQbsxwaLK92d~BpfaB5Fcc~F4wbuOoAUbyOKXgg1iOdlUR4a-bgaTnwXoEcTSp5qLdxytoTuppPRJx-kMIfetTAnbd2yXd4pF4S~0sBBUt0XIRd1if-4NA__'),
+    return Column(
+      children: [
+        AspectRatio(
+          aspectRatio: 160 / 138,
+          child: Container(
+            padding: EdgeInsets.only(right: 16, top: 16),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(24)),
+              image: customCachedNetworkImageprovider('https://s3-alpha-sig.figma.com/img/e247/c4b6/2d9352afd2a1a76e569e14ab01475e34?Expires=1730073600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bd9bwpmnwwbSgd0vUgtaJleJUm7RDiypSaM9eUJQWDJCIOnBTh2oB~uPHkgomWPkdJWDTO6FDehTZC73gI73-54-pjflldmWM6DAxT8ajAKGp2mCMc8e3KcQaP9gaC8kpLyKhxTWLHukIVAOaje-iJONoTTayzNSZ6EeeTSG9JG1LtmVWNJyhrH-NT-Et3oZQnGR28CXVWthIJigfeYbFWiZ1oQ8iWKGxQbsxwaLK92d~BpfaB5Fcc~F4wbuOoAUbyOKXgg1iOdlUR4a-bgaTnwXoEcTSp5qLdxytoTuppPRJx-kMIfetTAnbd2yXd4pF4S~0sBBUt0XIRd1if-4NA__'),
+            ),
+            alignment: Alignment.topRight,
+          ),
         ),
-        alignment: Alignment.topRight,
-      ),
+        Container(
+          width: double.infinity,
+          padding: EdgeInsets.only(left: 8),
+          child: Text("Smart Watches", style: Styless.textBold18(context),))
+      ],
     );
   }
 }
--- lib/features/categories/presentation/widgets/sub_categories_view_body.dart
+++ lib/features/categories/presentation/widgets/sub_categories_view_body.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:quick_mart_app/core/widgets/product/custom_sub_categories_item.dart';
 
 class SubCategoriesViewBody extends StatelessWidget {
   const SubCategoriesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 10,
+          crossAxisSpacing: 10,
+          childAspectRatio: 1,
+        ),
+        itemBuilder: (context, index) {
+          return InkWell(
+            onTap: () {},
+            child: CustomSubCategoriesItem(),
+          );
+        },
+      ),
+    );
   }
 }
