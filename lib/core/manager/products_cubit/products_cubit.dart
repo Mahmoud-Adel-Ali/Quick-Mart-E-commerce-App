fdd310a3f8d520d0e3b371b26d27b80d8992932b
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-10-21 16:36:49 -0700

    create products cubit and complete his methods

---
+++ lib/core/manager/products_cubit/products_cubit.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quick_mart_app/core/manager/repo/product_repo_impl.dart';
+import 'package:quick_mart_app/core/models/product_model/product_model.dart';
+
+part 'products_state.dart';
+
+class ProductsCubit extends Cubit<ProductsState> {
+  ProductsCubit(this.productRepoImpl) : super(ProductsInitial());
+  final ProductRepoImpl productRepoImpl;
+  List<ProductModel> allProducts = [];
+  List<String> allcategories = [];
+  void getAllProducts() async {
+    emit(GetAllProductsLoading());
+    var response = await productRepoImpl.getAllProducts();
+    response.fold(
+      (error) => emit(GetAllProductsFailure(errorMessage: error)),
+      (products) {
+        allProducts = products;
+        emit(GetAllProductsSuccess(products: products));
+      },
+    );
+  }
+
+  void getCategories() async {
+    emit(GetAllCategoriesLoading());
+    var response = await productRepoImpl.getCatergories();
+    response.fold(
+      (error) => emit(GetAllCategoriesFailure(errorMessage: error)),
+      (categories) {
+        allcategories = categories;
+        emit(GetAllCategoriesSuccess(categories: categories));
+      },
+    );
+  }
+
+  List<ProductModel> getProductsInCategory(String categoryId) {
+    if (allProducts.isEmpty) {
+      return [];
+    } else {
+      return allProducts
+          .where((product) => product.category == categoryId)
+          .toList();
+    }
+  }
+}
---
+++ lib/core/manager/products_cubit/products_state.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: must_be_immutable
+
+part of 'products_cubit.dart';
+
+@immutable
+sealed class ProductsState {}
+
+final class ProductsInitial extends ProductsState {}
+
+// get all products state
+final class GetAllProductsLoading extends ProductsState {}
+
+final class GetAllProductsFailure extends ProductsState {
+  final String errorMessage;
+  GetAllProductsFailure({required this.errorMessage});
+}
+
+final class GetAllProductsSuccess extends ProductsState {
+  GetAllProductsSuccess({required this.products});
+  List<ProductModel> products;
+}
+
+// get all categories
+final class GetAllCategoriesLoading extends ProductsState {}
+
+final class GetAllCategoriesFailure extends ProductsState {
+  final String errorMessage;
+  GetAllCategoriesFailure({required this.errorMessage});
+}
+
+final class GetAllCategoriesSuccess extends ProductsState {
+  GetAllCategoriesSuccess({required this.categories});
+  List<String> categories;
+}
+
+// get products in Category
+final class GetProductsInCategoryLoading extends ProductsState {}
+
+final class GetProductsInCategoryFailure extends ProductsState {
+  final String errorMessage;
+  GetProductsInCategoryFailure({required this.errorMessage});
+}
+
+final class GetProductsInCategorySuccess extends ProductsState {
+  GetProductsInCategorySuccess({required this.products});
+  List<ProductModel> products;
+}
