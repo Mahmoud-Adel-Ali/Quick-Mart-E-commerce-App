4dcf7edc556988e376baa67b3d5d08c0f5052d3b
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-10-21 21:54:48 -0700

    categories view logic done

--- lib/features/categories/presentation/widgets/categories_view_body.dart
+++ lib/features/categories/presentation/widgets/categories_view_body.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:quick_mart_app/core/manager/products_cubit/products_cubit.dart';
+import 'package:quick_mart_app/core/models/product_model/product_model.dart';
 import 'package:quick_mart_app/core/utils/app_routes.dart';
+import 'package:quick_mart_app/core/widgets/product/custom_category_card.dart';
 
 class CategoriesViewBody extends StatelessWidget {
   const CategoriesViewBody({super.key});
@@ -15,13 +19,18 @@ class CategoriesViewBody extends StatelessWidget {
           crossAxisSpacing: 10,
           childAspectRatio: 1.5,
         ),
+        itemCount: context.read<ProductsCubit>().allcategories.length,
         itemBuilder: (context, index) {
+          String category = context.read<ProductsCubit>().allcategories[index];
+          Map<String, List<ProductModel>> categoryMap =
+              context.read<ProductsCubit>().categoryMap;
           return InkWell(
             onTap: () {
-              Navigator.pushNamed(context, AppRoutes.subCategoriesView);
+              Navigator.pushNamed(context, AppRoutes.productListingView);
             },
-            // child: CustomCategoryCard(
-            //     categoryName: "Electronics", img: Assets.imagesCategory),
+            child: CustomCategoryCard(
+                categoryName: category,
+                imgPath: categoryMap[category]?.last.image ?? ""),
           );
         },
       ),
