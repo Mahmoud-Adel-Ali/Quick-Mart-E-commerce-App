e89e2d1c1d8f57c6247c8aeace8dbb3b0049d56e
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-01-30 00:20:32 -0800

    change the link of banner

--- lib/core/widgets/product/home_banner_only_item.dart
+++ lib/core/widgets/product/home_banner_only_item.dart
@@ -17,7 +17,7 @@ class HomeBannerOnlyItem extends StatelessWidget {
         decoration: const BoxDecoration(
           image: DecorationImage(
             image: NetworkImage(
-              'https://s3-alpha-sig.figma.com/img/cc61/3cbf/1b8131ced9da4b0d651e5a8d437e5eb2?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iJQuEeiKyEYCF65-QwKDUUvTElb7WKJ~wnBcRQUjKvv03eJpX~dofA73SItCHHgeqJooVh29NJql8JIdKGoMzTv6SRI2g7aR9j6z4gEX~~Kg6eDh3CqbT3Xy8Z-90VfvuOAqn1b3tuCzn2FqSk9UvpGroY80q-WIH6esQyQTlr2ZEQLhfyQyLDIHCLNt3Fpd-T60RESbAM~ejh6bV1nH65cZSQThxOv3e76hweu9HqRywJjzc9Z9YsncbICQvw6QpelrZ8fISF44dHp7GwaaXNnKFRczgPtdFZr7EI~Iww08nDd5mVgOHTwnHKBxGvNEojvWfsqqbRGcnV-1TQowQg__',
+              'https://s3-alpha-sig.figma.com/img/cc61/3cbf/1b8131ced9da4b0d651e5a8d437e5eb2?Expires=1739145600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=N~gqdTj4BrAw8azluAjNQJ-Yx9nHr8rS0fgyF0jIMS5IuQ6Q87ANRRvJjnGsJSRaeB7UDdqYo8GCddu1rtObKLwZmbSuWiuDZybnZCp73BhaxvIuAhaj0K~JkQBpS8dwzLf-YUpde33jQ678qFKjbB18ElvYse~Jz6B~0Dhi9nsSxUBNY0tYxipyQaitRLUQXunfHMNNAA2DVrbDieQdpixCbDRifLUnVi4SOUE7rpYYia3M7wBX3HzgUaJGf0~xl4LrPHqm5rWfaG2dZIF3OGipM60NFAYn-AqZEAgr8Ave-OSxabM13vgktsTYIreGaTDUfEZxCU4HKk4D3zvTxA__',
             ),
             fit: BoxFit.fill,
           ),
