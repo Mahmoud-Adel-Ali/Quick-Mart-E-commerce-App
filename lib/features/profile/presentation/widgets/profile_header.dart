2ea7faedd22fb9e07e07af884e23f52a5bd60497
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-12 12:13:23 -0800

    logout done

--- lib/features/profile/presentation/widgets/profile_header.dart
+++ lib/features/profile/presentation/widgets/profile_header.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:quick_mart_app/core/app/cubit/app_cubit.dart';
 import 'package:quick_mart_app/core/extensions/context_extention.dart';
 import 'package:quick_mart_app/core/utils/app_images.dart';
 import 'package:quick_mart_app/core/utils/styles.dart';
+
+import '../../../../core/utils/app_routes.dart';
 
 class ProfileHeader extends StatelessWidget {
   const ProfileHeader({super.key});
@@ -29,7 +33,76 @@ class ProfileHeader extends StatelessWidget {
         ),
         child: Image.asset(Assets.imagesCategory),
       ),
-      trailing: SvgPicture.asset(Assets.imagesLogoutIcon),
+      trailing: GestureDetector(
+        onTap: () {
+          // show simple dialog context logout | cancel
+          logoutDialog(context);
+        },
+        child: SvgPicture.asset(Assets.imagesLogoutIcon),
+      ),
+    );
+  }
+
+  Future<dynamic> logoutDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => SimpleDialog(
+        backgroundColor: context.color.mainColor,
+        title: SizedBox(
+          width: double.infinity,
+          child: Text(
+            'Are you sure you want to logout?',
+            style: Styless.textSemiBold18(context),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        children: [
+          SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor:
+                          WidgetStatePropertyAll(context.color.grey50),
+                    ),
+                    onPressed: () {
+                      context.read<AppCubit>().logout();
+                      context.pushNamedAndRemoveUntil(AppRoutes.login);
+                    },
+                    child: Text(
+                      'Logout',
+                      style: Styless.textSemiBold16(context),
+                    ),
+                  ),
+                ),
+                SizedBox(width: 8),
+                Expanded(
+                  child: ElevatedButton(
+                    onPressed: () {
+                      context.pop();
+                    },
+                    style: ButtonStyle(
+                      backgroundColor:
+                          WidgetStatePropertyAll(context.color.textColor),
+                    ),
+                    child: Text(
+                      'Cancel',
+                      style: Styless.textSemiBold16(context).copyWith(
+                        color: context.color.mainColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
