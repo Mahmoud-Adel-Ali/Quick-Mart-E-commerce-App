29a8d87ad42098906603e5e8a54641d21997dc04
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-13 04:12:16 -0800

    add bloc observer to track states

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:quick_mart_app/core/app/bloc_observer.dart';
 import 'package:quick_mart_app/core/app/cubit/app_cubit.dart';
 import 'package:quick_mart_app/core/databases/cach_keys.dart';
 import 'package:quick_mart_app/core/databases/my_cach-helper.dart';
@@ -10,14 +10,14 @@ import 'package:quick_mart_app/core/manager/products_cubit/products_cubit.dart';
 import 'package:quick_mart_app/core/manager/repo/product_repo_impl.dart';
 import 'package:quick_mart_app/core/services/services_locator.dart';
 import 'package:quick_mart_app/quick_mart.dart';
-import 'package:device_preview/device_preview.dart';
+import 'package:quick_mart_app/simple_bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //observer state of cubits
+  Bloc.observer = SimpleBlocObserver();
   setupServicesLocator();
   await getit<CacheHelper>().init();
-  //observer state of cubits
-  Bloc.observer = AppBlocObserver();
   // this step used to set device not rotated
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitDown,
