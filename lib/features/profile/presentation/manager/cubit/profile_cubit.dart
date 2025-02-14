5089d7a3362b9de54d303c9ce94b21a37e2ead4b
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-14 08:26:50 -0800

    create profile cubit

--- lib/features/profile/presentation/manager/cubit/profile_cubit.dart
+++ lib/features/profile/presentation/manager/cubit/profile_cubit.dart
@@ -2,8 +2,11 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 // ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
 
+import '../../../../auth/data/repos/auth_repo_implementation.dart';
+
 part 'profile_state.dart';
 
 class ProfileCubit extends Cubit<ProfileState> {
-  ProfileCubit() : super(ProfileInitial());
+  ProfileCubit({required this.authRepoImplementation}) : super(ProfileInitial());
+  final AuthRepoImplementation authRepoImplementation;
 }
--- lib/features/profile/presentation/view/profile_view.dart
+++ lib/features/profile/presentation/view/profile_view.dart
@@ -1,8 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quick_mart_app/core/extensions/context_extention.dart';
 import 'package:quick_mart_app/features/profile/presentation/widgets/profile_view_body.dart';
 
+import '../../../../core/api/dio_consumer.dart';
+import '../../../auth/data/repos/auth_repo_implementation.dart';
 import '../manager/cubit/profile_cubit.dart';
 import '../widgets/profile_header.dart';
 
@@ -12,7 +15,9 @@ class ProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider<ProfileCubit>(
-      create: (context) => ProfileCubit(),
+      create: (context) => ProfileCubit(
+          authRepoImplementation:
+              AuthRepoImplementation(dio: DioConsumer(dio: Dio()))),
       child: Scaffold(
         backgroundColor: context.color.cyan,
         body: const Column(
