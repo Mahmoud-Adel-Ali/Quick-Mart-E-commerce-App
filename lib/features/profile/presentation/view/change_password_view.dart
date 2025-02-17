dd490bebe7dbfd345ffcc8575ab16aac51386dad
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-17 11:29:10 -0800

    change password view body part 2

--- lib/features/profile/presentation/view/change_password_view.dart
+++ lib/features/profile/presentation/view/change_password_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../../core/widgets/app_logo.dart';
 import '../../../../core/widgets/custom_simple_app_bar.dart';
+import '../widgets/change_password_view_body.dart';
 
 class ChangePasswordView extends StatelessWidget {
   const ChangePasswordView({super.key});
@@ -14,6 +15,7 @@ class ChangePasswordView extends StatelessWidget {
         title: const AppLogo(),
         showLeading: true,
       ),
+      body: ChangePasswordViewBody(),
     );
   }
 }
