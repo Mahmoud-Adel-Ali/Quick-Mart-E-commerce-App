0bd5fdde18810bb21c18f6a5dbeb7a0160d796d2
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-18 12:43:45 -0800

    handle multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,13 @@ import 'package:quick_mart_app/core/services/services_locator.dart';
 import 'package:quick_mart_app/quick_mart.dart';
 import 'package:quick_mart_app/simple_bloc_observer.dart';
 
+import 'core/manager/products_cubit/products_cubit.dart';
+import 'core/manager/repo/product_repo_impl.dart';
+import 'features/auth/data/repos/auth_repo_implementation.dart';
+import 'features/home/data/repos/home_repo_impl.dart';
+import 'features/home/presentation/manager/home_cubit/home_cubit.dart';
+import 'features/profile/presentation/manager/cubit/profile_cubit.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //observer state of cubits
@@ -20,20 +27,40 @@ void main() async {
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitDown,
     DeviceOrientation.portraitUp,
-  ]).then((_) {
-    runApp(MultiBlocProvider(
-      providers: [
-        BlocProvider<AppCubit>(
-          create: (context) => AppCubit()
-            ..changeAppThemeMode(
-              sharedMode: CacheHelper().getBoolean(CachKeys.isDarkMode),
+  ]).then(
+    (_) {
+      runApp(
+        MultiBlocProvider(
+          providers: [
+            BlocProvider<AppCubit>(
+              create: (context) => AppCubit()
+                ..changeAppThemeMode(
+                  sharedMode: CacheHelper().getBoolean(CachKeys.isDarkMode),
+                ),
             ),
+            BlocProvider<HomeCubit>(
+              create: (context) =>
+                  HomeCubit(homeRepoImpl: getit.get<HomeRepoImpl>())
+                    ..getBannerData(),
+            ),
+            BlocProvider<ProfileCubit>(
+              create: (context) => ProfileCubit(
+                  authRepoImplementation: getit.get<AuthRepoImplementation>())
+                ..getUserProfile(),
+            ),
+            BlocProvider<ProductsCubit>(
+              create: (context) =>
+                  ProductsCubit(productRepoImpl: ProductRepoImpl())
+                    ..getAllProducts()
+                    ..getCategories(),
+            ),
+          ],
+          child: DevicePreview(
+            enabled: !kReleaseMode,
+            builder: (context) => const QuickMart(), // Wrap your app
+          ),
         ),
-      ],
-      child: DevicePreview(
-        enabled: !kReleaseMode,
-        builder: (context) => const QuickMart(), // Wrap your app
-      ),
-    ));
-  });
+      );
+    },
+  );
 }
