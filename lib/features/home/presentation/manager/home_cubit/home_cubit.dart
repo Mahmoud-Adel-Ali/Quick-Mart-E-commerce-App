4725426960f6b442cb391f028970cfb36395f276
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-18 11:34:03 -0800

    create get banner data in home cubit

--- lib/features/home/presentation/manager/home_cubit/home_cubit.dart
+++ lib/features/home/presentation/manager/home_cubit/home_cubit.dart
@@ -2,8 +2,25 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 // ignore: depend_on_referenced_packages
 import 'package:meta/meta.dart';
 
+import '../../../data/models/banner_model/banner_model/banner_data.dart';
+import '../../../data/repos/home_repo_impl.dart';
+
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeInitial());
+  HomeCubit({required this.homeRepoImpl}) : super(HomeInitial());
+
+  final HomeRepoImpl homeRepoImpl;
+
+  List<BannerData> bannerData = [];
+
+  Future<void> getBannerData() async {
+    emit(GetBannerDataLoadin());
+    final result = await homeRepoImpl.getBannerData();
+    result.fold((l) => emit(GetBannerDataError(errorMessage: l)),
+        (bannerModel) {
+      bannerData = bannerModel.data ?? [];
+      emit(GetBannerDataSuccess());
+    });
+  }
 }
--- lib/features/home/presentation/manager/home_cubit/home_state.dart
+++ lib/features/home/presentation/manager/home_cubit/home_state.dart
@@ -4,3 +4,14 @@ part of 'home_cubit.dart';
 sealed class HomeState {}
 
 final class HomeInitial extends HomeState {}
+
+// get banner states
+final class GetBannerDataLoadin extends HomeState {}
+
+final class GetBannerDataSuccess extends HomeState {}
+
+final class GetBannerDataError extends HomeState {
+  final String errorMessage;
+
+  GetBannerDataError({required this.errorMessage});
+}
