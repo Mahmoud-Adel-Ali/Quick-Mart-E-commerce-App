f624db8d992aaa076ea1ed4746b5cfce29ed9e27
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-02-20 04:29:40 -0800

    refactor product rating

--- lib/features/product_details/presentation/widgets/product_rate.dart
+++ lib/features/product_details/presentation/widgets/product_rate.dart
@@ -3,6 +3,8 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:quick_mart_app/core/widgets/product/custom_stars_rating_of_product_details.dart';
 
+import '../../../../core/utils/styles.dart';
+
 class ProductRate extends StatelessWidget {
   const ProductRate({super.key});
 
@@ -12,8 +14,14 @@ class ProductRate extends StatelessWidget {
     return Row(
       children: [
         CustomStarsRatingOfProductDetails(rating: myRandom),
-        Text(myRandom.toString().substring(0, 3)),
-        Text(' (${(myRandom * 256).toInt().toString()} Reviews)'),
+        Text(
+          myRandom.toString().substring(0, 3),
+          style: Styless.textSemiBold16(context),
+        ),
+        Text(
+          '     (${(myRandom * 256).toInt().toString()} Reviews)',
+          style: Styless.textSemiBold16(context),
+        ),
       ],
     );
   }
