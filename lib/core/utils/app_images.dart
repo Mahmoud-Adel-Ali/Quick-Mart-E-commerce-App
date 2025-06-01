721d33fc6daade080d9f02ffdbfcba386ba818da
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-05-31 21:56:50 -0700

    Checkout Header Icons

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -2,192 +2,118 @@
 class Assets {
   Assets._();
   
-  /// Assets for imagesCategory
-  /// assets/images/Category.png
   static const String imagesCategory = "assets/images/Category.png";
 
-  /// Assets for imagesCategoryCyan
-  /// assets/images/category-cyan.svg
   static const String imagesCategoryCyan = "assets/images/category-cyan.svg";
 
-  /// Assets for imagesCategoryGrey
-  /// assets/images/category-grey.svg
   static const String imagesCategoryGrey = "assets/images/category-grey.svg";
 
-  /// Assets for imagesCategoryWhite
-  /// assets/images/category-white.svg
   static const String imagesCategoryWhite = "assets/images/category-white.svg";
 
-  /// Assets for imagesChangePassword
-  /// assets/images/change_password.svg
   static const String imagesChangePassword = "assets/images/change_password.svg";
 
-  /// Assets for imagesDarkTheme
-  /// assets/images/dark_theme.svg
   static const String imagesDarkTheme = "assets/images/dark_theme.svg";
 
-  /// Assets for imagesEditProfile
-  /// assets/images/edit_profile.svg
   static const String imagesEditProfile = "assets/images/edit_profile.svg";
 
-  /// Assets for imagesEmptyCart
-  /// assets/images/empty_cart.png
   static const String imagesEmptyCart = "assets/images/empty_cart.png";
 
-  /// Assets for imagesError
-  /// assets/images/Error.svg
   static const String imagesError = "assets/images/Error.svg";
 
-  /// Assets for imagesFaqs
-  /// assets/images/faqs.svg
   static const String imagesFaqs = "assets/images/faqs.svg";
 
-  /// Assets for imagesGoogle
-  /// assets/images/google.svg
   static const String imagesGoogle = "assets/images/google.svg";
 
-  /// Assets for imagesHeartCyan
-  /// assets/images/heart-cyan.svg
   static const String imagesHeartCyan = "assets/images/heart-cyan.svg";
 
-  /// Assets for imagesHeartGrey
-  /// assets/images/heart-grey.svg
   static const String imagesHeartGrey = "assets/images/heart-grey.svg";
 
-  /// Assets for imagesHeartWhite
-  /// assets/images/heart-white.svg
   static const String imagesHeartWhite = "assets/images/heart-white.svg";
 
-  /// Assets for imagesHomeCyan
-  /// assets/images/home-cyan.svg
   static const String imagesHomeCyan = "assets/images/home-cyan.svg";
 
-  /// Assets for imagesHomeGrey
-  /// assets/images/home-grey.svg
   static const String imagesHomeGrey = "assets/images/home-grey.svg";
 
-  /// Assets for imagesHomeWhite
-  /// assets/images/home-white.svg
   static const String imagesHomeWhite = "assets/images/home-white.svg";
 
-  /// Assets for imagesInfo
-  /// assets/images/Info.svg
   static const String imagesInfo = "assets/images/Info.svg";
 
-  /// Assets for imagesLogoutIcon
-  /// assets/images/logout_icon.svg
   static const String imagesLogoutIcon = "assets/images/logout_icon.svg";
 
-  /// Assets for imagesMyCartCyan
-  /// assets/images/my-cart-cyan.svg
   static const String imagesMyCartCyan = "assets/images/my-cart-cyan.svg";
 
-  /// Assets for imagesMyCartGrey
-  /// assets/images/my-cart-grey.svg
   static const String imagesMyCartGrey = "assets/images/my-cart-grey.svg";
 
-  /// Assets for imagesMyCartWhite
-  /// assets/images/my-cart-white.svg
   static const String imagesMyCartWhite = "assets/images/my-cart-white.svg";
 
-  /// Assets for imagesOnBoarding1
-  /// assets/images/OnBoarding1.png
   static const String imagesOnBoarding1 = "assets/images/OnBoarding1.png";
 
-  /// Assets for imagesOnBoarding2
-  /// assets/images/OnBoarding2.png
   static const String imagesOnBoarding2 = "assets/images/OnBoarding2.png";
 
-  /// Assets for imagesOnBoarding3
-  /// assets/images/OnBoarding3.png
   static const String imagesOnBoarding3 = "assets/images/OnBoarding3.png";
 
-  /// Assets for imagesOrderHistory
-  /// assets/images/order_history.svg
   static const String imagesOrderHistory = "assets/images/order_history.svg";
 
-  /// Assets for imagesPageUnderBuild
-  /// assets/images/page_under_build.jpg
   static const String imagesPageUnderBuild = "assets/images/page_under_build.jpg";
 
-  /// Assets for imagesPasswordCreadedSuccess
-  /// assets/images/password_creaded_success.png
   static const String imagesPasswordCreadedSuccess = "assets/images/password_creaded_success.png";
 
-  /// Assets for imagesPaymentMethod
-  /// assets/images/payment_method.svg
+  static const String imagesPaymentBlackIcon = "assets/images/payment_black_icon.svg";
+
+  static const String imagesPaymentCyanIcon = "assets/images/payment_cyan_icon.svg";
+
+  static const String imagesPaymentGrayIcon = "assets/images/payment_gray_icon.svg";
+
   static const String imagesPaymentMethod = "assets/images/payment_method.svg";
 
-  /// Assets for imagesPrivacyPolicy
-  /// assets/images/privacy_policy.svg
+  static const String imagesPaymentWhiteIcon = "assets/images/payment_white_icon.svg";
+
   static const String imagesPrivacyPolicy = "assets/images/privacy_policy.svg";
 
-  /// Assets for imagesProfileCyan
-  /// assets/images/profile-cyan.svg
   static const String imagesProfileCyan = "assets/images/profile-cyan.svg";
 
-  /// Assets for imagesProfileGrey
-  /// assets/images/profile-grey.svg
   static const String imagesProfileGrey = "assets/images/profile-grey.svg";
 
-  /// Assets for imagesProfileWhite
-  /// assets/images/profile-white.svg
   static const String imagesProfileWhite = "assets/images/profile-white.svg";
 
-  /// Assets for imagesQ
-  /// assets/images/Q.png
   static const String imagesQ = "assets/images/Q.png";
 
-  /// Assets for imagesQuickmartDarkMode
-  /// assets/images/quickmart_dark_mode.png
   static const String imagesQuickmartDarkMode = "assets/images/quickmart_dark_mode.png";
 
-  /// Assets for imagesQuickmartLightMode
-  /// assets/images/quickmart_light_mode.png
   static const String imagesQuickmartLightMode = "assets/images/quickmart_light_mode.png";
 
-  /// Assets for imagesSearchBlack
-  /// assets/images/search-black.svg
+  static const String imagesReviewBlackIcon = "assets/images/review_black_icon.svg";
+
+  static const String imagesReviewGrayIcon = "assets/images/review_gray_icon.svg";
+
+  static const String imagesReviewWhiteIcon = "assets/images/review_white_icon.svg";
+
   static const String imagesSearchBlack = "assets/images/search-black.svg";
 
-  /// Assets for imagesSearchGrey
-  /// assets/images/search-grey.svg
   static const String imagesSearchGrey = "assets/images/search-grey.svg";
 
-  /// Assets for imagesSearchWhite
-  /// assets/images/search-white.svg
   static const String imagesSearchWhite = "assets/images/search-white.svg";
 
-  /// Assets for imagesShippingAddress
-  /// assets/images/shipping_address.svg
   static const String imagesShippingAddress = "assets/images/shipping_address.svg";
 
-  /// Assets for imagesSuccess
-  /// assets/images/Success.svg
+  static const String imagesShippingBlackIcon = "assets/images/shipping_black_icon.svg";
+
+  static const String imagesShippingCyanIcon = "assets/images/shipping_cyan_icon.svg";
+
+  static const String imagesShippingWhiteIcon = "assets/images/shipping_white_icon.svg";
+
   static const String imagesSuccess = "assets/images/Success.svg";
 
-  /// Assets for imagesSuffSearchBlack
-  /// assets/images/suff_search_black.svg
   static const String imagesSuffSearchBlack = "assets/images/suff_search_black.svg";
 
-  /// Assets for imagesSuffSearchGrey
-  /// assets/images/suff_search_grey.svg
   static const String imagesSuffSearchGrey = "assets/images/suff_search_grey.svg";
 
-  /// Assets for imagesSuffSearchWhite
-  /// assets/images/suff_search_white.svg
   static const String imagesSuffSearchWhite = "assets/images/suff_search_white.svg";
 
-  /// Assets for imagesTermsAndConditions
-  /// assets/images/terms_and_conditions.svg
   static const String imagesTermsAndConditions = "assets/images/terms_and_conditions.svg";
 
-  /// Assets for imagesTestCartImage
-  /// assets/images/test_cart_image.png
   static const String imagesTestCartImage = "assets/images/test_cart_image.png";
 
-  /// Assets for imagesWarning
-  /// assets/images/Warning.svg
   static const String imagesWarning = "assets/images/Warning.svg";
 }
 
