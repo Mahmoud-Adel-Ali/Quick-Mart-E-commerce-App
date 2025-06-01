06e166ce0aa5efcd41792cf2e0db57672982cb4d
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-05-31 19:01:53 -0700

    Refactor checkoutAppBar

--- lib/features/checkout/presentation/views/widgets/checkout_app.dart
+++ lib/features/checkout/presentation/views/widgets/checkout_app.dart
@@ -1,15 +1,19 @@
-  import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 import '../../../../../core/utils/styles.dart';
 import '../../../../../core/widgets/custom_simple_app_bar.dart';
 
 checkoutAppBar(BuildContext context) {
-    customAppBar(
-      context,
-      showLeading: true,
-      title: Text(
-        "Checkout",
-        style: Styless.textSemiBold16(context),
-      ),
-    );
-  }
+  return customAppBar(
+    context,
+    showLeading: true,
+    onTapOnLeading: () {
+      //if it is first view
+      Navigator.of(context).pop();
+    },
+    title: Text(
+      "Checkout",
+      style: Styless.textSemiBold16(context),
+    ),
+  );
+}
