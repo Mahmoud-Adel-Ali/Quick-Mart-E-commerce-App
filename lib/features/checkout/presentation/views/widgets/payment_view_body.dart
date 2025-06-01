a52f49035e952f9d70ef0ea412cb3ab2147d36ea
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-05-31 19:16:57 -0700

    Create Checkout View Body

---
+++ lib/features/checkout/presentation/views/widgets/payment_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PaymentViewBody extends StatelessWidget {
+  const PaymentViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Payment View Body"),
+    );
+  }
+}
